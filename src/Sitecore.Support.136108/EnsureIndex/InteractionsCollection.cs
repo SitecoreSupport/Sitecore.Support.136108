59de338e483956b33df3a7278c9563b370804149
Sergey Plashenko <spl@sitecore.net>
2017-11-24 11:39:51 +0200

    Initial commit

---
+++ src/Sitecore.Support.136108/EnsureIndex/InteractionsCollection.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Analytics.Data.DataAccess;
+using Sitecore.Analytics.Data.DataAccess.MongoDb;
+using Sitecore.Configuration;
+using Sitecore.Pipelines;
+
+namespace Sitecore.Support.EnsureIndex
+{
+  public class InteractionsCollection
+  {
+    public void Process(PipelineArgs args)
+    {
+      string str = DataAdapterManager.ConnectionStrings.Collection;
+      string[] textArray1 = new string[] { str };
+      MongoDbDriver driver = Factory.CreateObject("mongo/driver", textArray1, true) as MongoDbDriver;
+      string[] strArray = new string[] { "ContactId", "_t", "StartDateTime", "_id" };
+      driver.Interactions.EnsureIndex(true, strArray);
+    }
+  }
+}
